8e56a7ae546fe6cf5525510325543d290d80b3c3
DrRed96 <cbillimoria@outlook.com>
2021-10-28 11:59:15 +1100

    Added weekly exp channel

--- src/statChannels.js
+++ src/statChannels.js
@@ -11,16 +11,13 @@ module.exports = (client, hypixel) => {
     const discordGuild = client.guilds.cache.get("600311056627269642");
     const guildMembersChannel = discordGuild.channels.cache.get("821560951677386762");
     const guildLevelChannel = discordGuild.channels.cache.get("821560990083842089");
-
-    hypixel.getGuild("name", "Wristspasm").then(guild => {
-        guildMembersChannel.setName(`Guild Members: ${guild.members.length}/125`);
-        guildLevelChannel.setName(`Guild Level: ${guild.level}`);
-    }).catch(console.error);
+    const guildWeeklyExpChannel = discordGuild.channels.cache.get("903085205051555850");
 
     setInterval(() => {
         hypixel.getGuild("name", "Wristspasm").then(guild => {
             guildMembersChannel.setName(`Guild Members: ${guild.members.length}/125`);
             guildLevelChannel.setName(`Guild Level: ${guild.level}`);
+            guildWeeklyExpChannel.setName(`Guild Weekly EXP: ${guild.totalWeeklyGexp}`);
         }).catch(console.error);
     }, 10000);
 
