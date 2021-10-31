cc96a1d13e5c5c517aab6d89f14524cb0d782432
DrRed96 <cbillimoria@outlook.com>
2021-10-31 23:44:51 +1100

    Added auto chain breaker

--- src/index.js
+++ src/index.js
@@ -59,13 +59,18 @@ client.on("interactionCreate", async interaction => {
 });
 
 client.on("message", (message) => {
-	if (message.member.roles.cache.has(cfg.cursed_role_id)) {
+	if (message.member.roles.cache.has(cfg.cursed_role_id) && message.channelId !== cfg.chain_channel_id) {
 		if (message.content.toLowerCase().startsWith("i'm") || message.content.toLowerCase().startsWith("im")) {
 			let msg = message.content.split(/ +/);
 			msg.shift();
 			msg = msg.join(" ");
 			message.channel.send(`Hi ${msg}! I'm Dad!\n<@${message.author.id}>`);
 		}
+	} else if (message.channelId === cfg.chain_channel_id) {
+		const messages = await message.channel.messages.fetch({limit: 2});
+		if (message.content !== messages.last().content) {
+			message.member.roles.add(message.guild.roles.cache.get(cfg.chain_breaker_role_id));
+		}	
 	}
 })
 
