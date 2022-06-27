ee2b65ea20555d240a5009ce4b4a559bee5aadc8
DrRed96 <61581327+RedRushy@users.noreply.github.com>
2022-06-27 23:41:15 +1000

    updated bot status

--- src/handlers/StateHandler.js
+++ src/handlers/StateHandler.js
@@ -5,7 +5,7 @@ class StateHandler {
   
     async onReady() {
       this.discord.app.log.discord('Client ready, logged in as ' + this.discord.client.user.tag)
-      this.discord.client.user.setActivity('Wrist Spasm', { type: 'WATCHING' })
+      this.discord.client.user.setActivity('/g join Wristspasm', { type: 'PLAYING' })
   
     }
   }
