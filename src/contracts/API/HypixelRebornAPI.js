5f3dd2507128636801008803b0333a116969651f
DuckySoLucky <robertkovac160@gmail.com>
2023-02-27 19:57:07 +0100

    feat: update

--- src/contracts/API/HypixelRebornAPI.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line
-const HypixelAPIReborn = require("hypixel-api-reborn");
-const config = require("../../../config.json");
-
-const hypixel = new HypixelAPIReborn.Client(config.api.hypixelAPIkey, {
-  cache: true,
-});
-
-module.exports = hypixel;
