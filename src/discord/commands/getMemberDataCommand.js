d29ff85a59b334955c3d8b6787bdbd533ca85c08
DuckySoLucky <robertkovac160@gmail.com>
2023-07-24 12:15:17 +0200

    fix

--- src/discord/commands/getMemberDataCommand.js
+++ src/discord/commands/getMemberDataCommand.js
@@ -1,6 +1,24 @@
 const { EmbedBuilder } = require("discord.js");
 const fs = require("fs");
-const { timeSince } = require("../../contracts/helperFunctions");
+
+function formatUnixTime(milliseconds) {
+  const seconds = Math.floor(milliseconds / 1000);
+
+  const weeks = Math.floor(seconds / (60 * 60 * 24 * 7));
+  const days = Math.floor((seconds % (60 * 60 * 24 * 7)) / (60 * 60 * 24));
+  const hours = Math.floor((seconds % (60 * 60 * 24)) / (60 * 60));
+  const minutes = Math.floor((seconds % (60 * 60)) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const timeArray = [];
+  if (weeks > 0) timeArray.push(`${weeks}w`);
+  if (days > 0) timeArray.push(`${days}d`);
+  if (hours > 0) timeArray.push(`${hours}h`);
+  if (minutes > 0) timeArray.push(`${minutes}m`);
+  if (remainingSeconds > 0) timeArray.push(`${remainingSeconds}s`);
+
+  return timeArray.join(" ");
+}
 
 module.exports = {
   name: "get-member-data",
@@ -18,12 +36,13 @@ module.exports = {
         const { username, joined, lastLogin, left, lastLogout, online, playtime, messages } = value;
 
         output[username] = {
+          username: username,
           uuid: uuid,
           joined: joined,
           left: left,
           online: `${online ? "Yes" : "No"}`,
-          playtime: `${timeSince(Date.now() - playtime)}`,
           playtimeUnix: playtime,
+          playtime: `${formatUnixTime(playtime)}`,
           lastLogin: `${new Date(lastLogin) ?? "Unknown"}`,
           lastLoginUnix: lastLogin,
           lastLogout: `${new Date(lastLogout) ?? "Unknown"}`,
@@ -35,7 +54,13 @@ module.exports = {
       await interaction.editReply({
         files: [
           {
-            attachment: Buffer.from(JSON.stringify(output, null, 2)),
+            attachment: Buffer.from(
+              JSON.stringify(
+                Object.values(output).sort((a, b) => b.playtimeUnix - a.playtimeUnix),
+                null,
+                2
+              )
+            ),
             name: "memberData.json",
           },
         ],
