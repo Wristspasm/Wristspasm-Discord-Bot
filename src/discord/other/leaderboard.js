b9f84d3f184a715cd7f1793db53e21226d10c746
Kath <admin@kath.lol>
2024-05-31 10:51:15 +0800

    fix missing profile

--- src/discord/other/leaderboard.js
+++ src/discord/other/leaderboard.js
@@ -55,9 +55,13 @@ async function update() {
   guild.members.forEach((member) => uuids.push(member.uuid));
   const scores = [];
   for (let i = 0; i < uuids.length; i++) {
+    // check if the user has a saved file
     await delay(500);
     const data = await getLatestProfile(uuids[i]);
-    const oldProfile = JSON.parse(fs.readFileSync(`./latest/${uuids[i]}.json`));
+    if (!fs.existsSync(`latest/${uuids[i]}.json`)) {
+      fs.writeFileSync(`latest/${uuids[i]}.json`, JSON.stringify(data);
+    }
+    const oldProfile = JSON.parse(fs.readFileSync(`latest/${uuids[i]}.json`));
     const newScore =
       (data.profile.player_stats?.mythos?.burrows_dug_combat?.total || 0) +
       (data.profile.player_stats?.mythos?.burrows_dug_treasure?.total || 0);
